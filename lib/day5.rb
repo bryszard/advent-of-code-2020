5b8345d381217af42d216efe1e1510956ab606d1
Piotr Brych <pbrych@gmail.com>
2020-12-11 15:56:51 +0100

    Try another solution for Day5

--- lib/day5.rb
+++ lib/day5.rb
@@ -18,32 +18,33 @@ class Day5
     end
 
     def calculate_seat_code(input)
-      row = decode_row(input[0, 7])
-      column = decode_column(input[7, 3])
+      input.tr("FLBR", "0011").to_i(2)
+      # row = decode_row(input[0, 7])
+      # column = decode_column(input[7, 3])
 
-      row * 8 + column
+      # row * 8 + column
     end
 
-    private
+    # private
 
-    def decode_row(row_code)
-      bisect(row_code, "F", "B", TOTAL_ROWS)
-    end
+    # def decode_row(row_code)
+    #   bisect(row_code, "F", "B", TOTAL_ROWS)
+    # end
 
-    def decode_column(column_code)
-      bisect(column_code, "L", "R", TOTAL_COLUMNS)
-    end
+    # def decode_column(column_code)
+    #   bisect(column_code, "L", "R", TOTAL_COLUMNS)
+    # end
 
-    def bisect(input, first_code, last_code, total)
-      all_elements = 0..(total - 1)
+    # def bisect(input, first_code, last_code, total)
+    #   all_elements = 0..(total - 1)
 
-      input.chars.inject(all_elements) do |aggr, letter|
-        group_size = aggr.count / 2
-        method = :first if letter.eql?(first_code)
-        method = :last if letter.eql?(last_code)
+    #   input.chars.inject(all_elements) do |aggr, letter|
+    #     group_size = aggr.count / 2
+    #     method = :first if letter.eql?(first_code)
+    #     method = :last if letter.eql?(last_code)
 
-        aggr.send(method, group_size)
-      end.first
-    end
+    #     aggr.send(method, group_size)
+    #   end.first
+    # end
   end
 end
